fdd39edc8f6ee33348876198568a8fdbfb64a0c6
Melissa Clowdus <clowdusmelissa@gmail.com>
2023-02-18 02:45:44 -0600

    finished most of slider, work on routes next

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
 import Homepage from './Pages/Homepage/Homepage';
 import ThemeContextProvider from './contexts/ThemeContext';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <ThemeContextProvider>
         <Header />
-        <Homepage />
+        <Routes>
+            <Route path="/" element={<Homepage />} />
+        </Routes>
         <Footer />
       </ThemeContextProvider>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Slider.css'
 import axios from 'axios';
+import {MdKeyboardArrowRight, MdKeyboardArrowLeft} from 'react-icons/md'
 
 function Slider() {
 
@@ -9,6 +10,8 @@ function Slider() {
   const imageBase = process.env.REACT_APP_IMAGE_BASE;
 
   const [upcomingMovies, setUpcomingMovies] = React.useState([])
+
+  const [index, setIndex] =React.useState(0)
 
 //   https://api.themoviedb.org/3/movie/upcoming?api_key=a0fefc795663bf28827e25b186cb7b61
 
@@ -26,16 +29,47 @@ React.useEffect(
 const sliderStyle = {
     height: "60vh",
     width: "100%",
-    backgroundImage: `url("${imageBase}${upcomingMovies[0]?.backdrop_path}")`,
+    backgroundImage: `url("${imageBase}${upcomingMovies[index]?.backdrop_path}")`,
     backgroundRepeat: "no-repeat",
     backgroundSize: "cover",
     backgroundPosition: "center",
+    position: "relative", //need in order to use relative on other stuff
+}
+
+const handleRight = () =>{
+  console.log("right")
+  //check if at end of array
+  index === upcomingMovies.length-1?
+  setIndex(0)
+  :
+  setIndex(index+1)
+}
+
+const handleLeft = () =>{
+  //check if at the first element
+  index === 0 ?
+  setIndex(upcomingMovies.length-1)
+  :
+  setIndex(index-1)
 }
 
   return (
     <div className='slider-container'
     style={sliderStyle}>
-      {upcomingMovies[0]?.original_title}
+      <div className='slider-overlay'></div>
+      <MdKeyboardArrowLeft className='left-arrow'
+          onClick={handleLeft}/>
+      <MdKeyboardArrowRight className='right-arrow'
+          onClick={handleRight}/>
+      <div className='slider-movie-info'>
+        <h1>{upcomingMovies[index]?.original_title}</h1>
+        <p>{upcomingMovies[index]?.overview?.slice(0, 120)}</p>
+        <p>Genres: </p>
+        <p>Release Date: {upcomingMovies[index]?.release_date}</p>
+        <p>Rating: {upcomingMovies[index]?.vote_average}</p>
+        <p>See Details</p>
+      </div>
+      
     </div>
   )
 }
