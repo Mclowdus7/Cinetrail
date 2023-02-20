17d60be5b4f68b43607c0c35d2d465d0b8fc39d2
Melissa Clowdus <clowdusmelissa@gmail.com>
2023-02-19 22:26:27 -0600

    slider mostly done

--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import './Homepage.css'
 import Slider from '../../Components/Slider/Slider'
+import {ThemeContext} from '../../contexts/ThemeContext';
 
 function Homepage() {
+
+  //note use CURLY! brackets here to access global state
+
+  const {darkMode, setDarkMode} = useContext(ThemeContext)
 
   const apiKey = process.env.REACT_APP_API_KEY;
   const baseUrl = process.env.REACT_APP_BASE_URL;
@@ -12,7 +17,7 @@ function Homepage() {
   // console.log(imageBase);
 
   return (
-    <div className='homepage-container'>
+    <div className={darkMode? 'homepage-container': 'homepage-container homepage-light'}>
       <Slider />
       Homepage
     </div>
